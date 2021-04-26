6334546764830f09b2a9efdcbb31fe0b400546f6
Will Whitehead <willwhitehead@Wills-MacBook-Pro.local>
2021-04-27 09:57:37 +1000

    Added existing project files

---
+++ mysite/blog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Post
+
+
+# Register your models here.
+@admin.register(Post)  # admin.site.register(Post) is replaced by the decorator
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')  # The elements to display in the list.
+    list_filter = ('status', 'created', 'publish', 'author')  # The elements that can be filtered by.
+    search_fields = ('title', 'body')  # Shows a search bar that will search the  given fields.
+    date_hierarchy = 'publish'  # Shows a hierachy just below search box based on given field.
+    ordering = ('status', 'publish')  # Order of elements shown in panel.
+    prepopulated_fields = {'slug': ('title',)}  # Told Django to fill in the left element with the value on the right of the dict.
+    raw_id_fields = ('author', )  # Makes Django to build a lookup function.
---
+++ mysite/blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+# Create your views here.
+def post_list(request):
+    # posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # Cap the number of posts per page at 3.
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+
+    # return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
