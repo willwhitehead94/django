cb45d0ffc37a0173e615ae841054db2dc4c52fa5
= <=>
2021-04-27 11:00:37 +1000

    added class-based view for Post

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView  # Used for class-based views (Django specific)
 
 
 # Create your views here.
@@ -27,3 +28,10 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+# Class-based views, as opposed to functional views as above.
+class PostListView(ListView):
+    queryset = Post.published.all()
+    ontext_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
