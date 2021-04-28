e9f0c71451694238560649cee33e8da9c9be9392
Will Whitehead <will@littleoxfordstreet.com>
2021-04-28 14:43:10 +1000

    created initial Comment model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -42,3 +42,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post
+                            ,on_delete=models.CASCADE
+                            ,related_name='comments') #  If we didn't set this, it would be automatically become "model_set" defined by Django in the Post model.
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
