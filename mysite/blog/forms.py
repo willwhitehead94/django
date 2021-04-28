e64a7314d80d2f11204ee7e618e1cba173ab3a7e
Will Whitehead <will@littleoxfordstreet.com>
2021-04-28 15:23:30 +1000

    added comments to the view logic

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 
 # For more info:
 # https://docs.djangoproject.com/en/3.0/ref/forms/fields/
@@ -9,3 +10,8 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comment = forms.CharField(required=False, widget=forms.Textarea) #  Widgets override the default HTML, i.e. here we swap from Input to TextArea.
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
+        exclude = ('active',)
