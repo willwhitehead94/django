87dc49c24751395b77ad4295b2311ff31ead8a3c
Will Whitehead <will@littleoxfordstreet.com>
2021-04-29 22:38:31 +1000

    running using postgres

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "blog",
+        "USER": "blog",
+        "PASSWORD": "letmein1234",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
