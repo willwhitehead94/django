0128984214ae7ce3d44d26d08060fd775af011c0
Will Whitehead <will@littleoxfordstreet.com>
2021-05-01 15:30:47 +1000

    folder restructuring...

---
+++ project_social/djangovenv/bookmarks/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/willwhitehead/Documents/GitHub/django/project_social/djangovenv/bookmarks/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
