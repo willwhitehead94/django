d2de1c03318eebe4a85c220eb60a83ab974680be
Will Whitehead <will@littleoxfordstreet.com>
2021-05-01 15:47:28 +1000

    First commit of Chapter 4.

--- project_social/djangovenv/bookmarks/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/willwhitehead/Documents/GitHub/django/project_social/djangovenv/bookmarks/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
